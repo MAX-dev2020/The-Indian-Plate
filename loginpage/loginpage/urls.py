abd11e9c78f2da32473610e984cd723a45922668
Shankarling Kalashetty <66006314+shankarling2000@users.noreply.github.com>
2022-03-12 21:16:15 +0530

    Update urls.py

--- loginpage/loginpage/urls.py
+++ loginpage/loginpage/urls.py
@@ -23,7 +23,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls'))
-
+    path('', include('accounts.urls')),
+     url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':  settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
