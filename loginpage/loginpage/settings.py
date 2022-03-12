334eac17f4d437bb27616bdf4d324e35224fa342
Shankarling Kalashetty <66006314+shankarling2000@users.noreply.github.com>
2022-03-12 22:16:17 +0530

    Update settings.py

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = "app-root/repo/wsgi/static"
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
