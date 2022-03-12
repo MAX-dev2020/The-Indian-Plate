5a3a1fb9fc34f39e4daffc244202d43f3fa9554a
Shankarling Kalashetty <66006314+shankarling2000@users.noreply.github.com>
2022-03-12 22:19:51 +0530

    Update settings.py

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
