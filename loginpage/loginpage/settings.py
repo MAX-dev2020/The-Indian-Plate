8b9383603a45c61c096a06b3213691ec2b2074da
Shankarling Kalashetty <66006314+shankarling2000@users.noreply.github.com>
2022-03-12 21:34:35 +0530

    Update settings.py

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'ah(rlcun+r_e)pxi3dsb=*u!=fz9m&5@*pu9w1$972rg585pd7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'theindianplate.herokuapp.com']
 
 
 # Application definition
