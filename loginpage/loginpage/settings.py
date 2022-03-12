574bd98484e1f6e0987142943740fbe31387d698
Shankarling Kalashetty <66006314+shankarling2000@users.noreply.github.com>
2022-03-12 21:15:23 +0530

    Update settings.py

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ah(rlcun+r_e)pxi3dsb=*u!=fz9m&5@*pu9w1$972rg585pd7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
