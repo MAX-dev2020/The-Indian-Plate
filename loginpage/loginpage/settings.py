9f4f1891840ed04d26791a6fb0aaa1db684cb855
Shankarling Kalashetty <66006314+shankarling2000@users.noreply.github.com>
2022-03-12 22:46:11 +0530

    Update settings.py

--- loginpage/loginpage/settings.py
+++ loginpage/loginpage/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = "THE-INDIAN-PLATE\loginpage\loginpage\static"
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
